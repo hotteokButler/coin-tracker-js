0b9b441921d491c7d3ccc4e6907ce8420e5a6890
jisooyoon <ygs12567@gmail.com>
2022-04-09 00:04:07 +0900

    Add Apexchart test

--- src/routes/chart.tsx
+++ src/routes/chart.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import { useOutletContext } from 'react-router-dom';
 import { fetchCoinHistory } from '../service/coinApi';
+import ApexChart from 'react-apexcharts';
+import { Loader } from './coins';
 
 interface IChartProps {
   coinId: string;
@@ -10,10 +12,10 @@ interface IChartProps {
 interface ICoinHistory {
   time_open: string;
   time_close: string;
-  open: number;
-  high: number;
-  low: number;
-  close: number;
+  open: number; //시작가
+  high: number; //최고가
+  low: number; //최저가
+  close: number; //종가
   volume: number;
   market_cap: number;
   coinId?: string;
@@ -21,11 +23,70 @@ interface ICoinHistory {
 
 const Chart = () => {
   const coinId = useOutletContext<IChartProps['coinId']>();
-  const { isLoading, data } = useQuery<ICoinHistory>(['ohlcv', coinId], () =>
+  const { isLoading, data } = useQuery<ICoinHistory[]>(['ohlcv', coinId], () =>
     fetchCoinHistory(coinId!)
   );
-
-  return <div>Chart</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <Loader>Loading chart...</Loader>
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: 'Close Price', //
+              data: data?.map((price) => price.close) ?? [], //
+            },
+          ]}
+          options={{
+            theme: { mode: 'dark' }, //
+            chart: {
+              height: '100%', //
+              width: '300px', //
+              toolbar: {
+                show: false,
+              }, //
+              background: 'transparent',
+            }, //
+            grid: {
+              show: false,
+            },
+            stroke: { curve: 'smooth' }, //
+            xaxis: {
+              type: 'datetime',
+              categories: data?.map((date) => date.time_open),
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: false,
+            },
+            fill: {
+              type: 'gradient',
+              colors: ['#fbe7c6s'],
+              gradient: {
+                gradientToColors: ['#ffaebc'],
+                stops: [0, 100],
+              },
+            },
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
